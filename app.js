607a74f2c96b8e54481a38ca8625d0e193752af8
nhamlq <nham.le@axonactive.com>
2020-10-15 12:36:01 +0700

    sperate routes

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express') //Import Express
+const bodyParser = require('body-parser') //Import json magical :D
 const logger = require('./utils/logs/log') //Import logger with winston
 const db = require('./utils/db/db') //Import DB using Mongo
-const bodyParser = require('body-parser') //Import json magical :D
+const userRoute = require('./routes/user/user')
+
 const app = express()
 const port =  process.env.PORT || 3000;
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/users', userRoute);
 
 app.get('/', (req, res) => {
-    logger.warn('Run');
-    res.send('Ty  Chuot');
+    res.send('Coffee Station home page');
 })
 
-app.listen(3000 , () =>{
+app.listen(port , () =>{
     logger.info('Run at port: '+  port);
 });
---
+++ routes/user/user.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router  = express.Router();
+
+var users = [
+	{id: 1, name: "User1", email: "user1@gmail.com", age: 31}, 
+	{id: 2, name: "User2", email: "user2@gmail.com", age: 20},
+	{id: 3, name: "User1", email: "user1.2@gmail.com", age: 25}
+];
+
+router .get("/", async (req, res) => {
+	res.send('Users');
+});
+
+router .post("/user", async (req, res) => {});
+router .get("/user/:id", async  (req, res) => {});
+router .put("/user/:id", async  (req, res) => {});
+
+
+module.exports = router ;
