a31385f619b8349c3b9d0487ce807f661dc6f3bf
nhamlq <nham.le@axonactive.com>
2020-10-15 12:44:59 +0700

    change utils to config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express') //Import Express
 const bodyParser = require('body-parser') //Import json magical :D
-const logger = require('./utils/logs/log') //Import logger with winston
-const db = require('./utils/db/db') //Import DB using Mongo
+const logger = require('./config/logs/log') //Import logger with winston
+const db = require('./config/db/db') //Import DB using Mongo
 const userRoute = require('./routes/user') //Import routers user
 const contactRoute = require('./routes/contact') //Import routers contact
 
