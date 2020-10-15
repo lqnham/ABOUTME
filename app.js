392f847cc470baceff199e4289ff7c660b40eb63
nhamlq <nham.le@axonactive.com>
2020-10-15 13:40:06 +0700

    change gv

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const logger = require('./config/logs/log') //Import logger with winston
 const db = require('./config/db/db') //Import DB using Mongo
 const userRoute = require('./routes/user') //Import routers user
 const contactRoute = require('./routes/contact') //Import routers contact
-require('dotenv').config({path: './config/.env'}); // import enviroment variables with custom path
+require('dotenv').config({path: './config/.env'}); 
 
 const app = express()
-const port =  process.env.PORT;
+const PORT = process.env.PORT
 
 //Process bodyparser
 app.use(bodyParser.json())
@@ -17,14 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', userRoute);
 app.use('/contact', contactRoute);
 
-
 //default home page
 app.get('/', (req, res) => {
-    res.send('Coffee Station home page' + process.env.DB_URL);
+    res.send('Coffee Station home page');
 })
 
 
 //port listening
-app.listen(port , () =>{
-    logger.info('Run at port: '+  port);
+app.listen(PORT , () =>{
+    logger.info('Run at port: '+  PORT);
 });
