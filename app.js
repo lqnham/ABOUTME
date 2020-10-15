f9962c780d074517b0045b077f7fabb35297207c
nhamlq <nham.le@axonactive.com>
2020-10-15 12:14:28 +0700

    refactor

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express') //Import Express
-const logger = require('./utils/log') //Import logger with winston
-const db = require('./utils/db') //Import DB using Mongo
-
+const logger = require('./utils/logs/log') //Import logger with winston
+const db = require('./utils/db/db') //Import DB using Mongo
+const bodyParser = require('body-parser') //Import json magical :D
 const app = express()
-const port = 3000 //defind port listen
+const port =  process.env.PORT || 3000;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     logger.warn('Run');
