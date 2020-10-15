29bf5cf416a192d866b770270a733a058e5e01af
nhamlq <nham.le@axonactive.com>
2020-10-15 12:40:34 +0700

    refactor instruction

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const express = require('express') //Import Express
 const bodyParser = require('body-parser') //Import json magical :D
 const logger = require('./utils/logs/log') //Import logger with winston
 const db = require('./utils/db/db') //Import DB using Mongo
-const userRoute = require('./routes/user/user')
+const userRoute = require('./routes/user') //Import routers user
+const contactRoute = require('./routes/contact') //Import routers contact
 
 const app = express()
 const port =  process.env.PORT || 3000;
 
+//Process bodyparser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Declare routes
 app.use('/users', userRoute);
+app.use('/contact', contactRoute);
 
+
+//default home page
 app.get('/', (req, res) => {
     res.send('Coffee Station home page');
 })
 
+
+
+//port listening
 app.listen(port , () =>{
     logger.info('Run at port: '+  port);
 });
