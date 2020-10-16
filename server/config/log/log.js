e0ba1e5cf04feaf7c35e1ff0feb3b6352db34b6f
nhamlq <nham.le@axonactive.com>
2020-10-16 17:02:03 +0700

    enhance logger

--- server/config/log/log.js
+++ server/config/log/log.js
@@ -1,13 +1,14 @@
-const moment = require('moment');
-const path = require('path');
+require('winston-daily-rotate-file')
+require('moment-timezone')
+require('path')
+const moment = require('moment')
 const winston = require('winston')
-const dailylog = require('winston-daily-rotate-file')
+const { createLogger, format } = require('winston')
+const { combine, timestamp,  printf } = format
 
-const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf } = format;
-
-const loggingFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
+const loggingFormat = printf(({ level, message, timestamp }) => {
+  const timeStamp = moment.tz(timestamp, "Asia/Ho_Chi_Minh").format("HH:mm:ss.SSS"); //Format timezone to +7
+  return `${timeStamp} ` +`${level}: ${message}`;
 });
 
 //separate file by date for combined log
@@ -37,7 +38,6 @@ const fileRotateTransportsInfo = new (winston.transports.DailyRotateFile)({
 const logger = createLogger({
   defaultMeta: { service: 'user-service' },
   format: combine(
-    label({ label: 'logger' }),
     timestamp(),
     loggingFormat
   ),
